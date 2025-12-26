6a8813dbec7d3f9bf5a29139b6d95b355493a9da
othomas555 <o.thomas555@gmail.com>
2025-12-26 17:11:03 +0000

    Create furniture.js

---
+++ data/furniture.js
@@ -0,0 +1,22 @@
+const furnitureItems = [
+  {
+    id: "sofa",
+    name: "Sofa",
+    description: "2–3 seater sofas",
+    price: 45,
+  },
+  {
+    id: "wardrobe",
+    name: "Wardrobe",
+    description: "Flat-pack or solid",
+    price: 40,
+  },
+  {
+    id: "mattress",
+    name: "Mattress",
+    description: "Single, double or king",
+    price: 30,
+  },
+];
+
+export default furnitureItems;
